c62fd26870a29ddadb5e5f17ed9c6927715f476e
Victor Huerta <victorhuerta@MacBook-Pro-de-Victor.local>
2025-09-25 15:44:56 -0300

    Primer commit de la app ProfilePilot

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+// Fix: Import 'process' to provide type definitions for process.cwd() and resolve TypeScript error.
+import process from 'process';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY)
+    }
+  }
 })
